4f62eaf39e39a307914007d5c17cfe479253332a
amendoxe <arturo_mendoza@outlook.com>
2022-11-28 15:04:49 -0600

    Changing error handling

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,6 +1,6 @@
 const loginForm = document.querySelector("[data-login]");
 const errorMark = document.querySelector("[data-login-error]");
-const errorMail = document.querySelector("[data-login-error-mail]");
+const errorMail = document.querySelector("[data-login-error-inputs]");
 const errorPassword = document.querySelector("[data-login-error-password]");
 const email = document.querySelector("[data-email]");
 const password = document.querySelector("[data-password]");
@@ -11,17 +11,31 @@ loginForm.addEventListener("submit", (e) => {
 
 function validaLogin(e) {
 	console.log("here");
+	let checkMail = "";
+	let checkPassword = "";
 	if (email.value == "something@hotmail.com") {
 		console.log("mail is correct");
-		//TODO redirecciona a todos los productos
+		checkMail = true;
 	} else {
 		email.value = "";
 		password.value = "";
-		console.log("something is wrang!!");
+		console.log("something is mail wrang!!");
 		errorMail.style.display = "block";
-		errorPassword.style.display = "block";
-
 		errorMark.style.borderColor = "orange";
 	}
+	if (password.value == "another") {
+		console.log("mail is correct");
+		checkPassword = true;
+	} else {
+		email.value = "";
+		password.value = "";
+		console.log("something is password wrang!!");
+		errorPassword.style.display = "block";
+		errorMark.style.borderColor = "orange";
+	}
+	if (checkPassword && checkMail) {
+		//TODO redirecciona a todos los productos
+	}
+
 	e.preventDefault();
 }
