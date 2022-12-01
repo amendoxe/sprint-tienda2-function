bab02b3c0e07a2364f144a5da25574c6d73b8097
amendoxe <arturo_mendoza@outlook.com>
2022-11-30 18:42:26 -0600

    Redirecciona s productos sólo si los campos están llenos

---
+++ js/productos-validaciones.js
@@ -0,0 +1,20 @@
+//Validando productos
+const productosForm = document.querySelector("[data-productos-form]");
+const productosNombre = document.querySelector("[data-productos-nombre]");
+const productosPrecio = document.querySelector("[data-productos-precio]");
+const productosDescripcion = document.querySelector("[data-productos-descripcion]");
+
+productosForm.addEventListener("submit", (ev) => {
+	console.log("here");
+	validaProducto(ev);
+});
+function validaProducto(ev) {
+	if (productosNombre && productosPrecio && productosDescripcion) {
+		console.log("si son");
+		//redireccionar
+		window.location.href = "/views/productos.html";
+	} else {
+		conso("no estan completos");
+	}
+	ev.preventDefault();
+}
