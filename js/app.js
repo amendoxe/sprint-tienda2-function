d1ebaf320c38bf893b256d8aaddf06a29d8d0d03
amendoxe <arturo_mendoza@outlook.com>
2022-12-01 16:53:44 -0600

    Validaciones de añadir producto done

--- js/app.js
+++ js/app.js
@@ -3,8 +3,6 @@ const inputs = document.querySelectorAll("input");
 
 inputs.forEach((input) => {
 	input.addEventListener("blur", (input) => {
-		console.log("target: ", input.target);
-		console.log("input: ", input);
-		valida(input);
+		valida(input.target);
 	});
 });
--- js/validaciones2.js
+++ js/validaciones2.js
@@ -1,19 +1,21 @@
 // validaciones "b"
 export function valida(input) {
-	console.log(input);
+	const tipoDeInput = input.dataset.tipo; // viene del data-tipo en cada input
+
 	if (input.validity.valid) {
 		input.parentElement.classList.remove("input-container--invalid");
-		console.log("es válido", input.parentElement);
-		// input.querySelector(".input-message-error").innerHTML = "";
+		console.log("es válido", input.validity);
+		input.parentElement.querySelector(".input-message-error").innerHTML = "";
 	} else {
 		input.parentElement.classList.add("input-container--invalid");
-		console.log("no es válido", input.parentElement);
+		console.log("no es válido", input.validity);
+		input.parentElement.querySelector(".input-message-error").innerHTML = mostrarMensajeDeError(tipoDeInput, input);
 		// input.parentElement.querySelector(".input-message-error").innerHTML = mostrarMensajeDeError(tipoDeInput, input);
 	}
 }
 const mensajesDeError = {
 	nombre: {
-		valueMissing: "Este campo no puede estar vacío"
+		valueMissing: "El campo nombre no puede estar vacío"
 	},
 	mensaje: {
 		valueMissing: "Este campo no puede estar vacío"
@@ -27,16 +29,31 @@ const mensajesDeError = {
 		pattenMismatch:
 			"Al menos 6 caracteres, máximo 12, debe contener una letra minúscula, una letra mayúscula, un número  y no pude contener caracteres especiales."
 	},
-	nombre_producto: {
-		valueMissing: "Este campo no puede estar vacío"
+	nombrep: {
+		valueMissing: "El campo nombre del producto no puede estar vacío"
 	},
-	precio_producto: {
-		valueMissing: "Este campo no puede estar vacío"
+	precio: {
+		valueMissing: "El campo precio del producto no puede estar vacío"
 	},
-	descripción_producto: {
-		valueMissing: "Este campo no puede estar vacío"
+	descripción: {
+		valueMissing: "El campo descripción del producto no puede estar vacío"
 	},
 	busqueda: {
-		valueMissing: "Este campo no puede estar vacío"
+		valueMissing: "Este campo de búsqueda no puede estar vacío"
 	}
 };
+//lista de nuestros tipos de errores ↑
+const tipoDeErrores = ["valueMissing", "typeMismatch", "pattenMismatch"];
+
+function mostrarMensajeDeError(tipoDeInput, input) {
+	let mensaje = "";
+	tipoDeErrores.forEach((error) => {
+		if (input.validity[error]) {
+			console.log("validity", input.validity);
+			console.log("error", error);
+			console.log(mensajesDeError[tipoDeInput][error]);
+			mensaje = mensajesDeError[tipoDeInput][error];
+		}
+	});
+	return mensaje;
+}
