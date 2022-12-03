e7503bc182d307610aa1823ca9d0dcd8ed18bd24
amendoxe <arturo_mendoza@outlook.com>
2022-12-02 22:08:46 -0600

    creates already product list

--- service/client-service.js
+++ service/client-service.js
@@ -1,6 +1,30 @@
+//Creando elemento
+const creaNuevaLinea = (imagen, nombre, precio) => {
+	const linea = document.createElement("div");
+	const contenido = `<div class="section__single-products">
+			<img src=".${imagen}" class="product-image" />
+			<p class="product-name">${nombre}</p>
+			<p class="product-price">$${precio}</p>
+			<a href="">
+				<p class="product-link">Ver producto</p>
+			</a>
+		</div>`;
+	linea.innerHTML = contenido;
+	return linea;
+};
+
+//Donde crear el elemento
+const listaDonde = document.querySelector("[data-productos-todos]");
+
+//fetch
 const listaDeProductos = () => fetch("http://localhost:3000/producto").then((response) => response.json());
 
 listaDeProductos().then((response) => {
 	const data = response;
 	console.log(data);
+	//ya teniendo nuestro html y donde lo queremos
+	data.forEach((producto) => {
+		const nuevaLinea = creaNuevaLinea(producto.imagen, producto.nombre, producto.precio);
+		listaDonde.appendChild(nuevaLinea);
+	});
 });
