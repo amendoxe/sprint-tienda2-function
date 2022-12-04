af31ce2f7f6d09a587128578e0ac0995fd21566e
amendoxe <arturo_mendoza@outlook.com>
2022-12-04 00:00:39 -0600

    Actualiza completado

--- controllers/actualizar-controller.js
+++ controllers/actualizar-controller.js
@@ -1,5 +1,7 @@
 import { clientServices } from "../service/client-service.js";
 
+const formulario = document.querySelector("[data-productos-form]");
+
 const obtenerInformacion = () => {
 	const url = new URL(window.location); //clase ya establecida en js, contiene searchParams
 	const id = url.searchParams.get("id"); //obtenemos el id
@@ -10,7 +12,7 @@ const obtenerInformacion = () => {
 	const nombre = document.querySelector("[data-productos-nombre]");
 	const precio = document.querySelector("[data-productos-precio]");
 	const descripcion = document.querySelector("[data-productos-descripcion]");
-	console.log(nombre, imagen);
+
 	clientServices.detalleProducto(id).then((perfil) => {
 		imagen.value = perfil.imagen;
 		nombre.value = perfil.nombre;
@@ -20,3 +22,17 @@ const obtenerInformacion = () => {
 };
 
 obtenerInformacion();
+
+formulario.addEventListener("submit", (evento) => {
+	evento.preventDefault();
+	const url = new URL(window.location);
+	const id = url.searchParams.get("id");
+
+	const imagen = document.querySelector("[data-imagen]").value;
+	const nombre = document.querySelector("[data-productos-nombre]").value;
+	const precio = document.querySelector("[data-productos-precio]").value;
+	const descripcion = document.querySelector("[data-productos-descripcion]").value;
+	clientServices.actualizarCliente(nombre, precio, descripcion, imagen, id).then(() => {
+		window.location.href = "/views/productos-add.html";
+	});
+});
--- service/client-service.js
+++ service/client-service.js
@@ -21,10 +21,22 @@ const detalleProducto = (id) => {
 	return fetch(`http://localhost:3000/producto/${id}`).then((re) => re.json());
 };
 
+//actualizando
+const actualizarCliente = (nombre, precio, descripcion, imagen, id) => {
+	return fetch(`http://localhost:3000/producto/${id}`, {
+		method: "PUT",
+		headers: { "Content-type": "application/json" },
+		body: JSON.stringify({ nombre, precio, descripcion, imagen })
+	})
+		.then((res) => res)
+		.catch((err) => console.log("error actualizarCliente", err));
+};
+
 //exportando funciones
 export const clientServices = {
 	listaDeProductos,
 	crearProducto,
 	eliminarProducto,
-	detalleProducto
+	detalleProducto,
+	actualizarCliente
 };
